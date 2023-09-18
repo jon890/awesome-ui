8e4fab6774b05d551718e4150eee6cc922354156
BiFoS <jon89071@gmail.com>
2023-09-18 12:10:36 +0900

    :lipstick: AppleUI: 스크롤 기본 베이스 구현

---
+++ apple-ui/js/main.js
@@ -0,0 +1,89 @@
+(() => {
+  let yOffset = 0; // window.scrollY 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치 (yOffset)보다 이전에 위치한 스크롤 섹션들을의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고 있는) 씬(scroll-section)
+
+  const sceneInfo = [
+    {
+      // 0
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-0"),
+      },
+    },
+    {
+      // 1
+      type: "normal",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-1"),
+      },
+    },
+    {
+      // 2
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-2"),
+      },
+    },
+    {
+      // 3
+      type: "sticky",
+      heightNum: 5, // 브라우저 높이의 5배로 scrollHeight 세팅
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-3"),
+      },
+    },
+  ];
+
+  function setLayout() {
+    // 각 스크롤 섹션의 높이 세팅
+    for (const scene of sceneInfo) {
+      scene.scrollHeight = scene.heightNum * window.innerHeight;
+      scene.objs.container.style.height = `${scene.scrollHeight}px`;
+    }
+
+    yOffset = window.screenY;
+
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= yOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
+  }
+
+  function scrollLoop() {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return;
+      currentScene--;
+      document.body.setAttribute("id", `show-scene-${currentScene}`);
+    }
+  }
+
+  window.addEventListener("load", setLayout);
+  window.addEventListener("resize", setLayout);
+  window.addEventListener("scroll", () => {
+    yOffset = window.scrollY;
+    scrollLoop();
+  });
+})();

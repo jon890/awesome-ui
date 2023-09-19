f23f6e8778434d5fa6b744e4d1e63e1b37e00532
BiFoS <jon89071@gmail.com>
2023-09-19 17:37:32 +0900

    :bug: AppleUI: 리사이즈 버그 수정

--- apple-ui/js/main.js
+++ apple-ui/js/main.js
@@ -668,10 +668,14 @@
       if (window.innerWidth > 900) {
         setLayout();
         sceneInfo[3].values.rectStartY = 0;
+        window.location.reload();
       }
     });
     window.addEventListener("orientationchange", () => {
-      setTimeout(setLayout, 500);
+      scrollTo(0, 0);
+      setTimeout(() => {
+        window.location.reload();
+      }, 500);
     });
     window.addEventListener("scroll", () => {
       yOffset = window.scrollY;

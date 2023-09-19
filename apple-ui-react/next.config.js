9c7ff6055f83dca5d34c87514ccec512121da9d3
BiFoS <jon89071@gmail.com>
2023-09-19 19:12:57 +0900

    :green_heart: Fix production assetPrefix

--- apple-ui-react/next.config.js
+++ apple-ui-react/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "export",
   distDir: "dist",
   assetPrefix:
-    process.env === "production"
+    process.env.NODE_ENV === "production"
       ? "https://jon890.github.io/awesome-ui/apple-ui-react/dist/"
       : undefined,
 };

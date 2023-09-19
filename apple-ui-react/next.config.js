98a421c1451f435c164e6d9f5b50d36b3dfe29a8
BiFoS <jon89071@gmail.com>
2023-09-19 19:07:52 +0900

    :green_heart: No jekyll

--- apple-ui-react/next.config.js
+++ apple-ui-react/next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   output: "export",
   distDir: "dist",
-  assetPrefix: "https://jon890.github.io/awesome-ui/apple-ui-react/dist/",
+  assetPrefix:
+    process.env === "production"
+      ? "https://jon890.github.io/awesome-ui/apple-ui-react/dist/"
+      : undefined,
 };
 
 module.exports = nextConfig;
